a18695cb400a65579f955fea2cb985bcd231fdbc
Valkride <venzovossen@outlook.com>
2025-06-24 15:22:42 +0200

    new file:   .github/workflows/build-test.yml 	new file:   .github/workflows/build.yml 	deleted:    HID-Host/build.bat 	deleted:    HID-Host/hid-host-simple.py 	deleted:    HID-Host/hid-host.spec 	deleted:    HID-Host/hook-hid.py

--- HID-Host/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing PyInstaller...
-pip install pyinstaller
-
-echo Building executable...
-pyinstaller --onefile --console --add-data "hid-config.json;." --hidden-import hid --hidden-import psutil --hidden-import GPUtil --hidden-import pycaw.pycaw --hidden-import comtypes --hidden-import win32pdh hid-host.py
-
-echo Build complete!
-pause
